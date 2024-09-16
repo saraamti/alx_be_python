22c52ea8d3b16edb9fbffbcb5307ee88e60f2689
sara amti <saraamti0@gmail.com>
2024-09-16 17:42:33 +0100

    task1

--- basic_operations.py
+++ basic_operations.py
@@ -1,14 +1,9 @@
-# Define the variables
-number1 = 10
-number2 = 5
-
-# Perform basic operations
+number1 = int(10)
+number2 = int(5)
 addition = number1 + number2
 subtraction = number1 - number2
 multiplication = number1 * number2
-
-# Print the results
-print(f"Addition of {number1} and {number2} is {addition}")
-print(f"Subtraction of {number1} and {number2} is {subtraction}")
-print(f"Multiplication of {number1} and {number2} is {multiplication}")
+print("Addition of" , number1, "and" , number2, "is" , addition)
+print("Subtraction of" , number1, "and" , number2, "is" , subtraction)
+print("Multiplication of" , number1, "and" , number2, "is" , multiplication)
 

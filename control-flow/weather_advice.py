2444c9ef1ba2c57f5cb40d5423e7f13ea67a0b92
sara amti <saraamti0@gmail.com>
2024-09-22 15:08:10 +0100

    Add weather advice functionality

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,10 +1,12 @@
-tem = int(input"What's the weather like today? (sunny/rainy/cold):")
-if tem = sunny
-    print ("Wear a t-shirt and sunglasses.")
-elif tem = rainy
-    print ("Don't forget your umbrella and a raincoat.")
-elif tem = cold
-    print ("Make sure to wear a warm coat and a scarf.")
-else 
-print ("Sorry, I don't have recommendations for this weather.")
+weather = input("What's the weather like today? (sunny/rainy/cold): ")
+
+if weather == "sunny":
+    print("Wear a t-shirt and sunglasses.")
+elif weather == "rainy":
+    print("Don't forget your umbrella and a raincoat.")
+elif weather == "cold":
+    print("Make sure to wear a warm coat and a scarf.")
+else:
+    print("Sorry, I don't have recommendations for this weather.")
+
 
